76f8c779a486a0c6eb81bf5530eb543e956c3e58
Leonhard Seidel <leonhardseidel@gmx.de>
2021-10-08 19:43:37 +0200

    can now nearly do the login procedure

--- hptlndir.py
+++ hptlndir.py
@@ -12,7 +12,6 @@ except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pandas"])
 finally:
     import pandas as pd
-    from pandas.core.series import Series
 
 try:
     import regex as re
@@ -20,6 +19,7 @@ except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "regex"])
 finally:
     import regex as re
+
 try:
     import openpyxl
 except ImportError:
@@ -31,6 +31,7 @@ except ImportError:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "requests_html"])
 finally:
     from requests_html import HTMLSession
+    import requests
 
 
 args = sys.argv  # 0 is the file itself, 1 the deciding factor
@@ -81,7 +82,7 @@ if os.path.exists(args[1]):
             )
             fin = open(f_name + ".txt", "r", encoding="utf-8", errors="ignore")
             adj_lines = []
-            fin.read
+            fin.read()
             for lines in fin.readlines():
                 lines = re.sub(r"\|{1}\n", "\n", lines)
                 lines = re.sub(r"\|* *\|", "|", lines)
@@ -119,16 +120,45 @@ elif len(args) > 2:
     )
     if languages.isin([lang]).any():
         session = HTMLSession()
-        # session.cookies.
-        response = session.get(
+        REQUESTURL = (
             "https://crowdin.com/translate/house-party/158/en-de?filter=basic&value=0"
         )
-        if response.status_code == 200:
+        LOGINURL = "https://accounts.crowdin.com/login"
+        login_page = session.get(LOGINURL)
+
+        if login_page.status_code == 200:
             print("Connected to crowdin.com")
-            response.html.render()
-            print(response.html.html)
-            button = response.html.find("editor_download_button")
-            print(button)
+            print("Snatching Tokens...")
+            login_page.html.render()
+            html_token: str = str(login_page.html.find("input")[5])
+            # print(html_token)
+            token = re.findall(r"[\w\d]*(?='>)", html_token)[0]
+            # print(login_page.html.html)
+            print(f"Your current login token is {token}")
+            payload = {
+                "domain": "",
+                "cname": "",
+                "continue": "/translate/house-party/186/en-de?filter=basic&value=0",
+                "locale": "en",
+                "intended": "/auth/token",
+                "_token": token,
+                "email_or_login": "hpdownloader",
+                "password": "HousePartyGame",
+            }
+            post = session.post(REQUESTURL, data=payload)
+            print("Logging in...")
+            print(post.content)
+            response = session.get(
+                "https://crowdin.com/translate/house-party/158/en-de?filter=basic&value=0",
+                # cookies=cookies,
+            )
+            if response.status_code == 200:
+                print("Login succesful!")
+                response.html.render()
+                # print(response.cookies)
+                # print(response.html.html)
+                button = response.html.find("editor_download_button")
+                print(button)
 
 
 else:
